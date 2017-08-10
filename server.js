74c6ac7ebb263e82533e623bc44a40d536b8a52e
Neville Mehta <ndesk1900@gmail.com>
2017-08-10 16:52:27 -0700

    apis setup

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
+
 var express = require('express');
+var mockApiMiddleware = require('connect-mock-api').middleware;
+var casual = require('casual').en_US;
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 var app = express();
  
 var compiler = webpack(webpackConfig);
- 
+
 app.use(express.static(__dirname + '/www'));
- 
+
+app.use(mockApiMiddleware({
+  baseUrl: '', //optional
+  endpoints: [
+      {
+          path: '/api/images/',
+          template: function() {
+              var arr = [];
+              for (var i = 1; i <= 6 ; i++) {
+                arr.push({
+                  id: i,
+                  title: casual.title,
+                  path: "images/" + i + ".jpg",
+                  description: casual.description
+                })
+              }
+              return arr;
+          }
+      }
+    ]
+}));
+
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
